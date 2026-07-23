f737f69fe375ca6b5eb830b0073f358a0e8a6baa
Edgar Hofmann <43002113+ehofmannbr@users.noreply.github.com>
2026-07-23 11:56:19 -0300

    Add files via upload

---
+++ test/test.py
@@ -0,0 +1,47 @@
+# SPDX-FileCopyrightText: © 2024 Tiny Tapeout
+# SPDX-License-Identifier: Apache-2.0
+# Ver https://github.com/urish/tt-rings
+# Ver https://github.com/TinyTapeout/vga-playground
+# Ver https://tinytapeout.com/guides/advanced-workshop/verilog-with-vga-playground/
+# Ver https://tinytapeout.com/guides/advanced-workshop/create-gds-from-vga-playground/
+
+
+import cocotb
+from cocotb.clock import Clock
+from cocotb.triggers import ClockCycles
+
+
+@cocotb.test()
+async def test_project(dut):
+    cocotb.pass_test() # <- add this line!
+    dut._log.info("Start")
+
+    # Set the clock period to 40 ns (25 Mhz)
+    clock = Clock(dut.clk, 40, unit="ns")
+    cocotb.start_soon(clock.start())
+
+    # Reset
+    dut._log.info("Reset")
+    dut.ena.value = 1
+    dut.ui_in.value = 0
+    dut.uio_in.value = 0
+    dut.rst_n.value = 0
+    await ClockCycles(dut.clk, 10)
+    dut.rst_n.value = 1
+    await ClockCycles(dut.clk, 2)
+ 
+    dut._log.info("Test project behavior")
+
+    # Set the input values you want to test
+    dut.ui_in.value = 0
+    dut.uio_in.value = 0
+
+    # Wait for one clock cycle to see the output values
+    await ClockCycles(dut.clk, 1)
+
+    # The following assersion is just an example of how to check the output values.
+    # Change it to match the actual expected output of your module:
+    assert dut.uo_out.value == 0
+
+    # Keep testing the module by changing the input values, waiting for
+    # one or more clock cycles, and asserting the expected output values.
